be93eda0e33352a05b5af247322d58554497723f
DitCoders <ditcoders@gmail.com>
2023-06-18 07:48:02 +0700

    penambahan operasi crud

--- app/Http/Controllers/crudc.php
+++ app/Http/Controllers/crudc.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\crudm;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class crudc extends Controller
 {
@@ -11,7 +13,8 @@ class crudc extends Controller
      */
     public function index()
     {
-        //
+        $data = crudm::get();
+        return response()->json($data);
     }
 
     /**
@@ -19,15 +22,22 @@ class crudc extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required|string|max:20',
+            'info' => 'required|string',
+        ]);
+        if ($validasi->fails()) {
+            return response()->json([
+                'message' => $validasi->errors()
+            ]);
+        }
+        $data = crudm::create([
+            'name' => $request->name,
+            'info' => $request->info
+        ]);
+        return response()->json([
+            'message' => 'Berhasil menambahkan data'
+        ]);
     }
 
     /**
@@ -35,7 +45,23 @@ class crudc extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $validasi = Validator::make($request->all(), [
+            'name' => 'required|string|max:20',
+            'info' => 'required|string'
+        ]);
+        if ($validasi->fails()) {
+            return response()->json([
+                'message' => $validasi->errors()
+            ]);
+        }
+        $data = crudm::find($id);
+        $data->update([
+            'name' => $request->name,
+            'info'  => $request->info
+        ]);
+        return response()->json([
+            'message' => 'Data berhasil diubah'
+        ]);
     }
 
     /**
@@ -43,6 +69,7 @@ class crudc extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $data = crudm::find($id);
+        $data->delete();
     }
 }
